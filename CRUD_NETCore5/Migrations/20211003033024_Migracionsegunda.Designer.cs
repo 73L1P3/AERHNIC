bfc10c9e2dc46a37286652c19325d89eec8f1da4
Aldo <aldorm6000@gmail.com>
2021-10-03 19:55:35 -0600

    test

--- CRUD_NETCore5/Migrations/20211003033024_Migracionsegunda.Designer.cs
+++ CRUD_NETCore5/Migrations/20211003033024_Migracionsegunda.Designer.cs
@@ -4,14 +4,16 @@ using CRUD_NETCore5.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CRUD_NETCore5.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211003033024_Migracionsegunda")]
+    partial class Migracionsegunda
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
